308e0f0732633e59be61cb38afe3ff4872bc88a5
Lars Erickson <36152119-lericksonm@users.noreply.replit.com>
2024-09-02 03:11:22 +0000

    Update Vite config to set build output directory to 'dist'

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // This is the directory Netlify will publish
+  },
   server: {
     host: '0.0.0.0',
   }
-})
+});
