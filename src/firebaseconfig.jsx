1ead4d10eaaa80391382f252169269b9da46c27f
Lars Erickson <36152119-lericksonm@users.noreply.replit.com>
2024-09-03 16:40:07 +0000

    configure variables

--- src/firebaseconfig.jsx
+++ src/firebaseconfig.jsx
@@ -8,12 +8,12 @@ import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_REACT_APP_API_KEY,
-  authDomain: "alwayshangryapp.firebaseapp.com",
-  projectId: "alwayshangryapp",
-  storageBucket: "alwayshangryapp.appspot.com",
-  messagingSenderId: "930061813500",
-  appId: "1:930061813500:web:709b76cc05cef202dd7979",
-  measurementId: "G-V61WDMMS8Z"
+  authDomain: import.meta.env.VITE_REACT_APP_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_REACT_APP_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_REACT_APP_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_REACT_APP_APP_ID,
+    measurementId: import.meta.env.VITE_REACT_APP_MEASUREMENT_ID
 };
 
 
--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,14 @@ export default defineConfig({
     host: '0.0.0.0',
   },
   // Use import.meta.env to properly handle environment variables in Vite
-  define: {
-    'import.meta.env': process.env,
+    define: {
+      'import.meta.env': {
+        VITE_REACT_APP_API_KEY: JSON.stringify(process.env.VITE_REACT_APP_API_KEY),
+        VITE_REACT_APP_AUTH_DOMAIN: JSON.stringify(process.env.VITE_REACT_APP_AUTH_DOMAIN),
+        VITE_REACT_APP_PROJECT_ID: JSON.stringify(process.env.VITE_REACT_APP_PROJECT_ID),
+        VITE_REACT_APP_STORAGE_BUCKET: JSON.stringify(process.env.VITE_REACT_APP_STORAGE_BUCKET),
+        VITE_REACT_APP_MESSAGING_SENDER_ID: JSON.stringify(process.env.VITE_REACT_APP_MESSAGING_SENDER_ID),
+        VITE_REACT_APP_APP_ID: JSON.stringify(process.env.VITE_REACT_APP_APP_ID),
+        VITE_REACT_APP_MEASUREMENT_ID: JSON.stringify(process.env.VITE_REACT_APP_MEASUREMENT_ID),
   }
 });
