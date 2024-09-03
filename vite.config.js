0330a93800fff30f29679a8ec0d1273f2ed6f766
Lars Erickson <36152119-lericksonm@users.noreply.replit.com>
2024-09-03 16:21:04 +0000

    configured vite and firebase env variables

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // This is the directory Netlify will publish
-    base: '/', // Adjust if deploying to a subdirectory
+    outDir: 'dist', // Ensure this directory matches your Netlify publish directory
+    base: '/', // Use base '/' unless deploying to a subdirectory
   },
   server: {
     host: '0.0.0.0',
   },
+  // Vite uses import.meta.env instead of process.env
   define: {
-      'process.env': process.env
-    }
+    'import.meta.env': process.env,
   }
 });
